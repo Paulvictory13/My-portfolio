698e61a123526a4cf70b3d57b8a5a965f269a1f4
Paulvictory13 <paulvictory13@gmail.com>
2024-10-13 00:53:28 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".ex", {
+    Strings:["Frontend Developer", "Web Developer", "Backend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
