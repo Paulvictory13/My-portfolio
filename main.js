1a48f0b59bf5cc5f9b2370245a1959b3fe893e50
Paul Victory <133525463+Paulvictory13@users.noreply.github.com>
2024-10-19 11:36:55 +0100

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".ex", {
-    Strings:["Frontend Developer", "Web Developer", "Backend Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
-
